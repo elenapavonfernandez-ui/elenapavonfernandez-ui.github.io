e7aef1b6badbe8372e693e1ec63b32fb2a753745
ElenaP <elenapavonfernandez@gmail.com>
2026-02-20 14:35:31 +0100

    ccambios header y type effect

--- src/components/home/header/typeEfect.jsx
+++ src/components/home/header/typeEfect.jsx
@@ -6,7 +6,7 @@ const TypewriterComponent = () => {
     <div className="notranslate mt-5 text-3xl font-bold">
       <Typewriter
         options={{
-          strings: ["Desarrollo Web", "Frontend", "Backend", "UX/UI"],
+          strings: ["Data Analyst", "Data Visualization"],
           autoStart: true,
           loop: true,
         }}
