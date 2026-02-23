1110d98fb42d2ddaafef128fc3e4e46e0e8e8d66
ElenaP <elenapavonfernandez@gmail.com>
2026-02-23 09:54:10 +0100

    proyectcard

--- src/components/links/copyMail.jsx
+++ src/components/links/copyMail.jsx
@@ -3,7 +3,7 @@ import { FaRegCopy } from "react-icons/fa";
 
 export default function EmailCopy() {
   const [copied, setCopied] = useState(false);
-  const email = "lautarodevelops@gmail.com";
+  const email = "elenapavonfernandez@gmail.com";
 
   const copyToClipboard = async () => {
     try {
@@ -20,7 +20,7 @@ export default function EmailCopy() {
       <h1 className="text-lg font-medium">{email}</h1>
       <button
         onClick={copyToClipboard}
-        className="flex items-center gap-2 cursor-pointer font-bold rounded-md border-none  px-2 py-1 text-sm bg-primary hover:bg-secondary drop-shadow-[2px_2px_0_#0debd8]"
+        className="flex items-center gap-2 cursor-pointer font-bold rounded-md border-none  px-2 py-1 text-sm bg-primary hover:bg-secondary drop-shadow-[2px_2px_0_#00ffab]"
       >
         <FaRegCopy className="text-gray-300" />
         {copied ? "Copiado!" : "Copiar"}
