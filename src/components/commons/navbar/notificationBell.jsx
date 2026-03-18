3a7982b813be304c07beebb912c7360a8db0f227
ElenaP <elenapavonfernandez@gmail.com>
2026-03-18 20:52:06 +0100

    cv y otros ajustes

--- src/components/commons/navbar/notificationBell.jsx
+++ src/components/commons/navbar/notificationBell.jsx
@@ -11,7 +11,7 @@ export default function NotificationBell() {
     { id: 1, text: "Nuevo proyecto publicado 🚀", read: false },
     {
       id: 2,
-      text: "Hey como va?, te comento que actualmente estoy formándome en sistemas Cloud",
+      text: "Hey como va?, te comento que actualmente estoy formándome en AWS",
       read: false,
     },
   ];
